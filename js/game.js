1f63e7473c5be3420542719a1fb8e88c32c2d6ee
joedono <fjzusi@gmail.com>
2018-07-05 15:09:44 -0400

    Built rudamentary scene

--- js/game.js
+++ js/game.js
@@ -2,6 +2,13 @@ var config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: {y: 300},
+      debug: false
+    }
+  },
   scene: {
     preload: preload,
     create: create,
@@ -11,14 +18,27 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-function preload() {
+var platforms;
 
+function preload() {
+  this.load.image('sky', 'asset/sky.png');
+  this.load.image('ground', 'asset/platform.png');
+  this.load.image('star', 'asset/star.png');
+  this.load.image('bomb', 'asset/bomb.png');
+  this.load.spritesheet('dude', 'asset/dude.png', { frameWidth: 32, frameHeight: 32 });
 }
 
 function create() {
+  this.add.image(400, 300, 'sky');
 
+  platforms = this.physics.add.staticGroup();
+  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+  platforms.create(600, 400, 'ground');
+  platforms.create(50, 250, 'ground');
+  platforms.create(750, 220, 'ground');
 }
 
 function update() {
-  
+
 }
