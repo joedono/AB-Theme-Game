9ba46a6a80175f4e15d30bc65b82e9adae0ec645
joedono <fjzusi@gmail.com>
2018-07-09 11:31:08 -0400

    Added config file

---
+++ js/config.js
@@ -0,0 +1,2 @@
+var SCREEN_WIDTH = 480;
+var SCREEN_HEIGHT = 480;
--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
 var config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: SCREEN_WIDTH,
+  height: SCREEN_HEIGHT,
   physics: {
     default: 'arcade',
     arcade: {
