fa7802a4ffb5a0d0d28ede40f671ca81fcdaae26
joedono <fjzusi@gmail.com>
2018-07-10 10:36:28 -0400

    Directly embedded the map tileset in the map

--- wip/floor.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Floor" tilewidth="8" tileheight="8" tilecount="4" columns="2">
- <image source="../asset/image/floor.png" trans="ffffff" width="16" height="16"/>
-</tileset>
