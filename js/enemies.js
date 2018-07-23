c12061eee0656f7960c8774e9063703641c95e2d
joedono <fjzusi@gmail.com>
2018-07-23 13:53:55 -0400

    Whitespace update

--- js/enemies.js
+++ js/enemies.js
@@ -1,116 +1,116 @@
 Enemies = function(game) {
-  this.game = game;
+	this.game = game;
 
-  game.anims.create({
-    key: 'enemyRight',
-    frames: [{ key: 'enemy', frame: 0 }],
-    frameRate: 20
-  });
+	game.anims.create({
+		key: 'enemyRight',
+		frames: [{ key: 'enemy', frame: 0 }],
+		frameRate: 20
+	});
 
-  game.anims.create({
-    key: 'enemyUp',
-    frames: [{ key: 'enemy', frame: 1 }],
-    frameRate: 20
-  });
+	game.anims.create({
+		key: 'enemyUp',
+		frames: [{ key: 'enemy', frame: 1 }],
+		frameRate: 20
+	});
 
-  game.anims.create({
-    key: 'enemyLeft',
-    frames: [{ key: 'enemy', frame: 2 }],
-    frameRate: 20
-  });
+	game.anims.create({
+		key: 'enemyLeft',
+		frames: [{ key: 'enemy', frame: 2 }],
+		frameRate: 20
+	});
 
-  game.anims.create({
-    key: 'enemyDown',
-    frames: [{ key: 'enemy', frame: 3 }],
-    frameRate: 20
-  });
+	game.anims.create({
+		key: 'enemyDown',
+		frames: [{ key: 'enemy', frame: 3 }],
+		frameRate: 20
+	});
 
-  this.enemies = game.physics.add.group();
+	this.enemies = game.physics.add.group();
 
-  this.buildPaths();
-  game.physics.add.collider(this.enemies, walls);
-  game.physics.add.collider(this.enemies, playerSword, strikeEnemy, null, game);
+	this.buildPaths();
+	game.physics.add.collider(this.enemies, walls);
+	game.physics.add.collider(this.enemies, playerSword, strikeEnemy, null, game);
 
-  this.spawnTimer = ENEMY_SPAWN_TIMER;
+	this.spawnTimer = ENEMY_SPAWN_TIMER;
 };
 
 Enemies.prototype = {
-  update: function(delta) {
-    delta = delta / 1000;
-    this.updateSpawnTimer(delta);
-    this.updateEnemies(delta);
-    this.cleanupEnemies();
-  },
+	update: function(delta) {
+		delta = delta / 1000;
+		this.updateSpawnTimer(delta);
+		this.updateEnemies(delta);
+		this.cleanupEnemies();
+	},
 
-  buildPaths: function() {
-    var paths = new Array();
+	buildPaths: function() {
+		var paths = new Array();
 
-    var pathSources = new Array();
-    pathSources.push(map.getObjectLayer('Paths Basic')['objects']);
-    pathSources.push(map.getObjectLayer('Paths Simple')['objects']);
-    pathSources.push(map.getObjectLayer('Paths Simple Longer')['objects']);
-    pathSources.push(map.getObjectLayer('Paths Medium Vertical')['objects']);
-    pathSources.push(map.getObjectLayer('Paths Medium Horizontal')['objects']);
-    pathSources.push(map.getObjectLayer('Paths Complex One')['objects']);
-    pathSources.push(map.getObjectLayer('Paths Complex Two')['objects']);
-    pathSources.push(map.getObjectLayer('Paths Complex Three')['objects']);
+		var pathSources = new Array();
+		pathSources.push(map.getObjectLayer('Paths Basic')['objects']);
+		pathSources.push(map.getObjectLayer('Paths Simple')['objects']);
+		pathSources.push(map.getObjectLayer('Paths Simple Longer')['objects']);
+		pathSources.push(map.getObjectLayer('Paths Medium Vertical')['objects']);
+		pathSources.push(map.getObjectLayer('Paths Medium Horizontal')['objects']);
+		pathSources.push(map.getObjectLayer('Paths Complex One')['objects']);
+		pathSources.push(map.getObjectLayer('Paths Complex Two')['objects']);
+		pathSources.push(map.getObjectLayer('Paths Complex Three')['objects']);
 
-    pathSources.forEach(function(pathSource) {
-      pathSource.forEach(function(line) {
-        var originX = line.x;
-        var originY = line.y;
-        var points = line.polyline;
-        var pathX = new Array();
-        var pathY = new Array();
-        points.forEach(function(point) {
-          pathX.push(originX + point.x);
-          pathY.push(originY + point.y);
-        });
+		pathSources.forEach(function(pathSource) {
+			pathSource.forEach(function(line) {
+				var originX = line.x;
+				var originY = line.y;
+				var points = line.polyline;
+				var pathX = new Array();
+				var pathY = new Array();
+				points.forEach(function(point) {
+					pathX.push(originX + point.x);
+					pathY.push(originY + point.y);
+				});
 
-        paths.push({
-          'x': pathX,
-          'y': pathY
-        });
-      });
-    });
+				paths.push({
+					'x': pathX,
+					'y': pathY
+				});
+			});
+		});
 
-    this.paths = paths;
-  },
+		this.paths = paths;
+	},
 
-  updateSpawnTimer: function(delta) {
-    this.spawnTimer -= delta;
-    if(this.spawnTimer <= 0) {
-      this.spawnEnemy();
-      this.spawnTimer = ENEMY_SPAWN_TIMER;
-    }
-  },
+	updateSpawnTimer: function(delta) {
+		this.spawnTimer -= delta;
+		if(this.spawnTimer <= 0) {
+			this.spawnEnemy();
+			this.spawnTimer = ENEMY_SPAWN_TIMER;
+		}
+	},
 
-  spawnEnemy: function() {
-    var path = this.paths[RANDOM];
-    var startPointX = path.x[0];
-    var startPointY = path.y[0];
+	spawnEnemy: function() {
+		var path = this.paths[RANDOM];
+		var startPointX = path.x[0];
+		var startPointY = path.y[0];
 
-    var enemy = this.enemies.add(startPointX, startPointY, 'enemy');
-    enemy.setData('health', ENEMY_HEALTH);
-    enemy.setData('path', path);
-    enemy.setData('timer', 0);
-  },
+		var enemy = this.enemies.add(startPointX, startPointY, 'enemy');
+		enemy.setData('health', ENEMY_HEALTH);
+		enemy.setData('path', path);
+		enemy.setData('timer', 0);
+	},
 
-  updateEnemies: function(delta) {
-    this.enemies.children.iterate(function(enemy) {
-      var timer = enemy.getData('timer');
-      timer += delta;
-      var progress = timer / ENEMY_PATH_TIMER;
+	updateEnemies: function(delta) {
+		this.enemies.children.iterate(function(enemy) {
+			var timer = enemy.getData('timer');
+			timer += delta;
+			var progress = timer / ENEMY_PATH_TIMER;
 
-      // TODO Move along path
-    });
-  },
+			// TODO Move along path
+		});
+	},
 
-  cleanupEnemies: function() {
-    this.enemies.children.iterate(function(enemy) {
-      if(enemy.getData('health') <= 0) {
-        enemy.destroy();
-      }
-    });
-  }
+	cleanupEnemies: function() {
+		this.enemies.children.iterate(function(enemy) {
+			if(enemy.getData('health') <= 0) {
+				enemy.destroy();
+			}
+		});
+	}
 };
--- js/game.js
+++ js/game.js
@@ -1,18 +1,18 @@
 var config = {
-  type: Phaser.AUTO,
-  width: SCREEN_WIDTH,
-  height: SCREEN_HEIGHT,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      debug: false
-    }
-  },
-  scene: {
-    preload: preload,
-    create: create,
-    update: update
-  }
+	type: Phaser.AUTO,
+	width: SCREEN_WIDTH,
+	height: SCREEN_HEIGHT,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			debug: false
+		}
+	},
+	scene: {
+		preload: preload,
+		create: create,
+		update: update
+	}
 };
 
 var game = new Phaser.Game(config);
@@ -28,48 +28,48 @@ var enemies;
 var cursors;
 
 function preload() {
-  this.load.tilemapTiledJSON('map', 'asset/config/map.json');
-  this.load.spritesheet('floor', 'asset/image/floor.png', { frameWidth: 8, frameHeight: 8 });
-  this.load.image('wall', 'asset/image/wall.png');
+	this.load.tilemapTiledJSON('map', 'asset/config/map.json');
+	this.load.spritesheet('floor', 'asset/image/floor.png', { frameWidth: 8, frameHeight: 8 });
+	this.load.image('wall', 'asset/image/wall.png');
 
-  this.load.spritesheet('player', 'asset/image/player.png', { frameWidth: 16, frameHeight: 16 });
-  this.load.spritesheet('enemy', 'asset/image/enemy.png', { frameWidth: 16, frameHeight: 16 });
+	this.load.spritesheet('player', 'asset/image/player.png', { frameWidth: 16, frameHeight: 16 });
+	this.load.spritesheet('enemy', 'asset/image/enemy.png', { frameWidth: 16, frameHeight: 16 });
 }
 
 function create() {
-  cursors = this.input.keyboard.createCursorKeys();
+	cursors = this.input.keyboard.createCursorKeys();
 
-  map = this.make.tilemap({ key: 'map' });
-  var floorTiles = map.addTilesetImage('Floor', 'floor');
-  floorLayer = map.createStaticLayer('Floor', floorTiles, 0, 0);
+	map = this.make.tilemap({ key: 'map' });
+	var floorTiles = map.addTilesetImage('Floor', 'floor');
+	floorLayer = map.createStaticLayer('Floor', floorTiles, 0, 0);
 
-  buildWalls(this);
+	buildWalls(this);
 
-  player = new Player(this);
-  enemies = new Enemies(this);
+	player = new Player(this);
+	enemies = new Enemies(this);
 
-  this.physics.add.collider(player.sprite, walls);
+	this.physics.add.collider(player.sprite, walls);
 }
 
 function buildWalls(game) {
-  var mapWalls = map.getObjectLayer('Walls')['objects'];
-  walls = game.physics.add.staticGroup();
-  mapWalls.forEach(function(mapWall) {
-    var wall = walls.create(mapWall.x, mapWall.y, 'wall');
-    wall.setScale(mapWall.width / 8, mapWall.height / 8);
-    wall.setOrigin(0);
-    wall.body.width = mapWall.width;
-    wall.body.height = mapWall.height;
-  });
+	var mapWalls = map.getObjectLayer('Walls')['objects'];
+	walls = game.physics.add.staticGroup();
+	mapWalls.forEach(function(mapWall) {
+		var wall = walls.create(mapWall.x, mapWall.y, 'wall');
+		wall.setScale(mapWall.width / 8, mapWall.height / 8);
+		wall.setOrigin(0);
+		wall.body.width = mapWall.width;
+		wall.body.height = mapWall.height;
+	});
 
-  walls.refresh();
+	walls.refresh();
 }
 
 function update(time, delta) {
-  player.update();
-  enemies.update(delta);
+	player.update();
+	enemies.update(delta);
 }
 
 function strikeEnemy(enemy, sword) {
-  enemy.setData('health', enemy.getData('health') - 1);
+	enemy.setData('health', enemy.getData('health') - 1);
 }
