39b195cf8556f4ce5fa6ce33744dd3339faf112d
joedono <fjzusi@gmail.com>
2019-04-02 14:49:25 +0100

    Re-initialized game to base

--- js/game.js
+++ js/game.js
@@ -8,40 +8,7 @@ var config = {
 		arcade: {
 			debug: false
 		}
-	},
-	scene: [scenePlaying, sceneGameOver]
+	}
 };
 
 var game = new Phaser.Game(config);
-
-var map;
-var floorLayer;
-var walls;
-
-var player;
-var playerSword;
-var family;
-var enemies;
-var score = 0;
-var scoreText;
-
-var cursors;
-
-function strikeEnemy(sword, enemy) {
-	var dead = enemy.getData('dead');
-
-	if(!dead) {
-		increaseScore();
-		enemy.setData('dead', true);
-	}
-}
-
-function increaseScore() {
-	score += 15;
-	scoreText.setText('Blood Spilled: ' + score);
-}
-
-function playerSwordSwingComplete(animation, frame) {
-	player.swingingSword = false;
-	playerSword.setPosition(-100, -100);
-}
